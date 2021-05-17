b7d1e6e93c43e1e8157b04f97a058b52954e6e13
Geewhan Kim <g1kim@ucsd.edu>
2021-05-16 19:43:19 -0700

    Added service workers

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,17 @@ const home = { name: 'home' };
 const settings = { name: 'settings' };
 
 // Make sure you register your service worker here too
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   let counter = 1;
